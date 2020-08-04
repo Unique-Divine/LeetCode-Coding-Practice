233360c778daf7bbe3549b26babf3caefa19f89f
Unique Divine <u.divine@columbia.edu>
2020-08-03 23:28:12 -0500

    Upload Anki Notebook

--- docstring_generator.py
+++ docstring_generator.py
@@ -19,3 +19,24 @@ def one_hot_encode(X, cat_features):
         X = pd.concat([X, hot_one.set_index(X.index)], axis=1)
     return X
 
+def display_tear_sheet(factor_data, factor_name):
+    """[summary]
+
+    Args:
+        factor_data ([type]): [description]
+        factor_name ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    # Get the 1-day forward returns for the assets and dates in the factor
+    returns_df = get_forward_returns(
+        factor_data[factor_name+' momentum_factor'],
+        [1], US_EQUITIES)
+
+    # Format the factor and returns data so that we can run it through Alphalens.
+    al_data = al.utils.get_clean_factor(
+        factor_data[factor_name+' momentum_factor'],
+        returns_df, quantiles=5, bins=None,)
+
+    return create_full_tear_sheet(al_data)
