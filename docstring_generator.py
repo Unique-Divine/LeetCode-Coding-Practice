29f4a94825d67909834d331baa75196715b179e1
Unique Divine <u.divine@columbia.edu>
2020-09-10 21:29:10 -0500

    add numpy helper function

--- docstring_generator.py
+++ docstring_generator.py
@@ -1,42 +1,50 @@
-    def getInfoGain(self, node, split_index):
-        """[summary]
+def display_attributes(obj):
+    """[summary]
 
-        Args:
-            node ([type]): [description]
-            split_index ([type]): [description]
+    Args:
+        obj ([type]): [description]
 
-        Returns:
-            [type]: [description]
-        """
+    Returns:
+        (str): The instance attributes of the object
+    """    
+    attributes = ', '.join(i for i in dir(obj) if not i.startswith('_'))
+    # source note: https://tinyurl.com/yyef5trl | stack overflow
+    return attributes
 
-        left_uncertainty = self.get_uncertainty(node.labels[:split_index]) 
-        right_uncertainty = self.get_uncertainty(node.labels[split_index:])
+
+def convertOutputToTrades(close_price, out_labels, capital=100000):
+    """[summary]
+
+    Returns:
+        [type]: [description]
+    """    
+    start_c = capital
+    num_shares = 0
+    total_return = 0
+    total_invested = 0
+    rev = 0
+    last_buy_prices = []
+    for i in range(len(out_labels)):
+        print('Num_shares = ', num_shares)
+        if out_labels[i] == 1 and capital >= close_price[i]:
+            num_shares += 1
+            capital -= close_price[i]
+            total_invested += close_price[i]
+            last_buy_prices.append(close_price[i])
+        elif out_labels[i] == 0 and num_shares > 0:
+            capital += num_shares*close_price[i]
+            rev += num_shares*close_price[i]
+            for price in last_buy_prices[-num_shares:]:
+                total_return += close_price[i] - price
+                #total_invested -= price
+            num_shares = 0
+    if num_shares > 0:
+        capital += num_shares*close_price[-1]
+        rev += num_shares*close_price[-1]
+        total_return += num_shares*(close_price[-1] - last_buy_prices[-1])
+        num_shares = 0
         
-        n = len(node.labels)
-        w1 = left_uncertainty * (split_index+1) / n
-        w2 = right_uncertainty * (n-(split_index+1))/n
-        
-        start_entropy = self.get_uncertainty(node.labels)
-        conditional_entropy = w1 + w2 
-        infogain = start_entropy - conditional_entropy
-        return infogain
-
-    def get_uncertainty(self, labels, metric="gini"):
-        """[summary]
-
-        Args:
-            labels ([type]): [description]
-            metric (str, optional): [description]. Defaults to "gini".
-
-        Returns:
-            [type]: [description]
-        """        
-        
-        if labels.shape[0] == 0:
-            return 1
-        if metric =="gini":    
-            uncertainty = gini(labels)
-        if metric == "entropy":
-            uncertainty = entropy(labels)
-        
-        return uncertainty
+    TR = (capital - start_c)/start_c
+    TR2 = (rev - total_invested)/total_invested
+    
+    return total_invested, rev, TR, TR2
