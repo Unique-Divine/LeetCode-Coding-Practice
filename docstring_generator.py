3e54aba558a68c7b85c9802186b182d6baf639e4
Unique Divine <u.divine@columbia.edu>
2020-11-12 23:22:24 -0600

    cleaned up unnecessary code

--- docstring_generator.py
+++ docstring_generator.py
@@ -1,50 +1,14 @@
-def display_attributes(obj):
-    """[summary]
+    def read(cls, ticker):
+        """[summary]
 
-    Args:
-        obj ([type]): [description]
+        Args:
+            ticker ([type]): [description]
 
-    Returns:
-        (str): The instance attributes of the object
-    """    
-    attributes = ', '.join(i for i in dir(obj) if not i.startswith('_'))
-    # source note: https://tinyurl.com/yyef5trl | stack overflow
-    return attributes
-
-
-def convertOutputToTrades(close_price, out_labels, capital=100000):
-    """[summary]
-
-    Returns:
-        [type]: [description]
-    """    
-    start_c = capital
-    num_shares = 0
-    total_return = 0
-    total_invested = 0
-    rev = 0
-    last_buy_prices = []
-    for i in range(len(out_labels)):
-        print('Num_shares = ', num_shares)
-        if out_labels[i] == 1 and capital >= close_price[i]:
-            num_shares += 1
-            capital -= close_price[i]
-            total_invested += close_price[i]
-            last_buy_prices.append(close_price[i])
-        elif out_labels[i] == 0 and num_shares > 0:
-            capital += num_shares*close_price[i]
-            rev += num_shares*close_price[i]
-            for price in last_buy_prices[-num_shares:]:
-                total_return += close_price[i] - price
-                #total_invested -= price
-            num_shares = 0
-    if num_shares > 0:
-        capital += num_shares*close_price[-1]
-        rev += num_shares*close_price[-1]
-        total_return += num_shares*(close_price[-1] - last_buy_prices[-1])
-        num_shares = 0
-        
-    TR = (capital - start_c)/start_c
-    TR2 = (rev - total_invested)/total_invested
-    
-    return total_invested, rev, TR, TR2
+        Returns:
+            [type]: [description]
+        """
+        original = pd.read_csv(os.path.join(data_path, file_prefix+ ticker + file_suffix))
+        dates = original['Date'].map(lambda t: datetime.strptime(t, '%Y-%m-%d')).values
+        prices = original['Open'].values
+        original['dates'] = dates
+        return cls(dates, prices, original)
